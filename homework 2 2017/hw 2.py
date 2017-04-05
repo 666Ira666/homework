2b1b99be9373c45a4dc2ddfd267c3d5287e9af16
IrinaPavlova <ispavlovais@gmail.com>
2017-04-05 13:29:02 +0300

    added a template

--- homework 2 2017/hw 2.py
+++ homework 2 2017/hw 2.py
@@ -3,5 +3,14 @@ __author__ = 'IrinaPavlova'
 from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
-with app.test_request_context():
-    print(url_for('static', filename='style.css'))
+@app.route('/')
+def home_page():
+    return render_template('home_page.html')
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
+
+if __name__ == "__main__":
+    app.run(debug=True)
