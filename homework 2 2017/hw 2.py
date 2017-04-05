64bac0e12478f9d949ea4179781a81f0e8d3c8c9
IrinaPavlova <ispavlovais@gmail.com>
2017-04-05 17:24:43 +0300

    adds

--- homework 2 2017/hw 2.py
+++ homework 2 2017/hw 2.py
@@ -12,8 +12,12 @@ def home():
 
 @app.route('/login', methods=['POST'])
 def do_admin_login():
-    if request.form['password'] == 'password' and request.form['username'] == 'admin':
-        session['logged_in'] = True
+    db = {'Timofey': 'tim123', 'admin': 'password'}
+    if request.form['username'] in db:
+        if request.form['password'] == db[request.form['username']]:
+            session['logged_in'] = True
+        else:
+            return render_template('home_page.html')
     else:
         return render_template('home_page.html')
     return home()
