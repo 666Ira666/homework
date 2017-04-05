ce6b9f562ada0d0ed98bd33c57004e9bde80ba6e
IrinaPavlova <ispavlovais@gmail.com>
2017-04-05 19:37:44 +0300

    adds

--- homework 2 2017/hw 2.py
+++ homework 2 2017/hw 2.py
@@ -3,12 +3,14 @@ import os
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
     if not session.get('logged_in'):
         return render_template('login.html')
     else:
         return render_template('home_page.html')
+
 
 @app.route('/login', methods=['POST'])
 def do_admin_login():
@@ -29,9 +31,17 @@ def logout():
     return home()
 
 
-@app.route("/search")
+@app.route("/search", methods=['GET', 'POST'])
 def search():
+    if request.method == "POST":
+        query = request.form['query']
+        return search_results(query)
     return render_template('search_page.html')
+
+
+@app.route("/search/results/<query>", methods=['GET', 'POST'])
+def search_results(q):
+    return render_template('search_query_page.html', q=q)
 
 if __name__ == "__main__":
     app.secret_key = os.urandom(12)
