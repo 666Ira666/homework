a5c6024ddb90ab422e85f7b47795c0b2fa988008
IrinaPavlova <ispavlovais@gmail.com>
2017-04-05 13:43:12 +0300

    added a template

--- homework 2 2017/hw 2.py
+++ homework 2 2017/hw 2.py
@@ -1,11 +1,28 @@
 __author__ = 'IrinaPavlova'
 
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, session, flash
 app = Flask(__name__)
 
+'''
 @app.route('/')
 def home_page():
     return render_template('home_page.html')
+'''
+
+@app.route('/')
+def home():
+    if not session.get('logged_in'):
+        return render_template('login.html')
+    else:
+        return "Hello Boss!"
+
+@app.route('/login', methods=['POST'])
+def do_admin_login():
+    if request.form['password'] == 'password' and request.form['username'] == 'admin':
+        session['logged_in'] = True
+    else:
+        flash('wrong password!')
+    return home()
 
 @app.route('/hello/')
 @app.route('/hello/<name>')
